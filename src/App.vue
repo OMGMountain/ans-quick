c89b51ed808e47813ef906508a663ebe6d84b204
wangmingjia <wangmingjia@analysys.com.cn>
2020-05-27 17:29:31 +0800

    JS

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+#app {
+  font-family: "PingFangSC-Light,Helvetica Neue,Helvetica,Microsoft Yahei,Arial,Hiragino Sans GB,tahoma,SimSun,sans-serif";
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import JavaScript from '@/pages/JavaScript/JavaScript'
+
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'JavaScript',
+            component: JavaScript
+        }
+    ]
+})
